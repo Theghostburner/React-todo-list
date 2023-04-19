ed5dce3078e254f04b9764b2aba7584d59657f7c
Sudeep Sharma <50044868+Theghostburner@users.noreply.github.com>
2023-04-19 19:30:16 +0530

    errors fixed and completed UI including statechanges for todo app

---
+++ react_todo_list/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import React, {useState} from 'react'
+import './App.css';
+import data from "./components/data.json";
+import Header from './components/Header';
+import ToDoList from "./components/ToDoList";
+import ToDoForm from "./components/ToDoForm";
+
+
+function App() {
+  const [ toDoList, setToDoList ] = useState(data)
+
+  const handleToggle = (id) => {
+    console.log("handleToggle Called");
+    let mapped = toDoList.map(task => {
+      return task.id == id ? {...task,complete: !task.complete} : {...task};
+    });
+    setToDoList(mapped);
+  }
+  const handleFilter = () => {
+    console.log("handleFilter Called");
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+  const addTask = (userInput) => {
+    console.log("addTask called");
+    let copy = [...toDoList];
+    copy = [...copy,{id:toDoList.length +1,task:userInput, complete:false}];
+    setToDoList(copy);
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+      <ToDoForm addTask={addTask}/>
+    </div>
+  );
+}
+
+export default App;
